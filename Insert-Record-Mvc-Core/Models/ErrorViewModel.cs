6175b760c7bb0c9644bba393b03a88cb30919832
Zaidrakhman Miras <miras.zaidrakhman@outlook.com>
2020-01-18 20:01:28 +0600

    Add project files.

---
+++ Insert-Record-Mvc-Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Insert_Record_Mvc_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
